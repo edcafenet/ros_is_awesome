7f08d82a53f6a1906bb537b1afc77f78b24f708f
Eddie <edcafenet@gmail.com>
2017-11-14 21:28:39 -0500

    added installtion script

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-mv ./ros_is_awesome/catkin_ws ~/
+cp -R ./ros_is_awesome/catkin_ws ~/
 cd ~/catkin_ws/ && catkin_make && source ~/catkin_ws/devel/setup.bash 
+rm -rf ~/ros_is_awesome
+cd ~/catkin_ws/src
